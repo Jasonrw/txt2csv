2688ff7b50614404274933aa12679c9dae2200be
jason <jason@0xng.com>
2017-05-26 20:05:34 +0800

    improve the auto detect function

--- autoClassify2.py
+++ autoClassify2.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+from __future__ import division
 import csv
 import codecs
 import shutil
 import os
+import sys
 from time import clock
 import glob
 import chardet
+from chardet.universaldetector import UniversalDetector
+from progressbar import Percentage, ProgressBar,Bar,ETA
 
 def utf_8_encoder(unicode_csv_data):
     for line in unicode_csv_data:
@@ -14,22 +18,71 @@ def utf_8_encoder(unicode_csv_data):
 
 def autoDetectEncoding(txtFilePath):
     with open(txtFilePath) as f:
-        bufferSize = 10240
+        detector = UniversalDetector()
+        whole_time = 300
         while True:
-            rawData = f.read(bufferSize)
-            encodingMethod = chardet.detect(rawData).get('encoding')
-            confidence =  chardet.detect(rawData).get('confidence')
-            print '='*50
-            print "file: " + txtFilePath 
-            print 'encodingMethod:' + encodingMethod 
-            print "confidence: " + str(confidence)
-            print 'bufferSize: ' + str(bufferSize)
-            print '='*50
-            if confidence > 0.9:
+            counter = 0
+            print '='*20 + "Auto detect encoding method!" + '='*20
+            #file_size = os.stat(txtFilePath).st_size
+            #print file_size
+            read_size = 0
+            print "file:" + txtFilePath
+            bar = ProgressBar(widgets=[Bar('=', '[', ']'), ' ', Percentage(), ' ', ETA()],
+                       maxval=1).start()
+            start_time = clock()
+            for line in f.readlines():
+                detector.feed(line)
+                #read_size += sys.getsizeof(line)
+                spend_time = clock() - start_time
+                bar.update(int(spend_time)/whole_time)
+                if spend_time > whole_time:
+                    break
+                #if counter%1000 == 0:
+                #    print "lineNum:" + str(counter)
+                #counter += 1
+                if detector.done: 
+                    break
+            print '='*20 + "Detecting done!"+ '='*20
+        #print float(detector.result['confidence'])
+        #print detector.done
+        #detector.close()
+            if not detector.done:
+                if f.readline():
+                    detector.close()
+                    if float(detector.result['confidence']) < 0.9:
+                        print detector.result
+                        print "Get more confidence!"
+                        detector.reset()
+                        f.seek(0)
+                        whole_time += 10
+                    else:
+                        break
+                else:
+                    break
+            else:
                 break
-            bufferSize *= 2
-            f.seek(0)
-        return encodingMethod
+                
+        f.close()
+        with open('./output/detecEncodingMethod.txt', 'a') as f1:
+            f1.write('\n' + txtFilePath + '\n' + str(detector.result))
+        return detector.result
+        #bufferSize = 10240
+        #while True:
+
+        #    rawData = f.read(bufferSize)
+        #    encodingMethod = chardet.detect(rawData).get('encoding')
+        #    confidence =  chardet.detect(rawData).get('confidence')
+        #    print '='*50
+        #    print "file: " + txtFilePath 
+        #    print 'encodingMethod:' + encodingMethod 
+        #    print "confidence: " + str(confidence)
+        #    print 'bufferSize: ' + str(bufferSize)
+        #    print '='*50
+        #    if confidence > 0.9:
+        #        break
+        #    bufferSize *= 2
+        #    f.seek(0)
+        #return encodingMethod
 
 #def guessEncodeMethod(rawData):
     #encodeMethods = ['utf-8',]
@@ -74,8 +127,9 @@ def autoClassify(src_root_path, target_dir):
                 if encoding not in encodingMethod:
                     encodingMethod.append(encoding)
                 #print encoding
-                print encodingMethod
+                #print encodingMethod
                 #txt2csv(txtFile, encoding)
 
 autoClassify("/media/jason/Seagate Backup Plus Drive/penetration/Dic/data/",
 "/media/jason/sgk/")
+#autoDetectEncoding("../duowan_user.txt")
